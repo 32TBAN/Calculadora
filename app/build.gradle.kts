cd55b9c9b969810154f45a336685ca6f134c1f4f
Kevin Guanoluisa <estebanguanoluisa55@gmail.com>
2023-11-19 15:16:28 -0500

    v0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "Esteban.G.myapplication"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "Esteban.G.myapplication"
--- app/src/main/java/Esteban/G/myapplication/MainActivity.java
+++ app/src/main/java/Esteban/G/myapplication/MainActivity.java
@@ -2,13 +2,46 @@ package Esteban.G.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+EditText editTextExprecion;
+Button deleteButton ;
+private  String operand="",result="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextExprecion = findViewById(R.id.etInput);
+        deleteButton = findViewById(R.id.btnDelete);
+        deleteButton.setEnabled(false);
     }
+
+    public void onClickButtons(View view) {
+        Button button = (Button) view;
+        operand += button.getText().toString();
+        editTextExprecion.setText(operand);
+        deleteButton.setEnabled(true);
+
+    }
+
+    public void onClickDelete(View view) {
+
+        operand = operand.substring(0,operand.length()-1);
+        editTextExprecion.setText(operand);
+
+        if (operand.isEmpty()){
+            deleteButton.setEnabled(false);
+        }
+    }
+
 }
